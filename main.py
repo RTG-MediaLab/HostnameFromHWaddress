4cc4901b80e427bff91e861e9da5faa0b11d0e36
Tobias Jensen <tobias@Pluto>
2017-12-01 09:42:50 +0100

    Fixed Dochange variable

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ if currentos == 'Linux':
 	print('Du bruger Linux') 
 	iface = 'wlp3s0'
 	mac = commands.getoutput('cat /sys/class/net/' + iface + '/address')
-	if dochange == 0:
+	if dochange == 1:
      		#Change hostname
 		os.system('sudo hostname ' + mac) #The hostname command is volatile, maybe we should consider something else?
 
@@ -32,13 +32,13 @@ elif currentos == 'Darwin': #Darwin is the name of the kernel that Apple uses fo
 	cutmac = os.popen('ifconfig ' + iface + ' |grep ether').read()
 	halfmac = cutmac.rsplit(' ')
 	mac = halfmac[1]
-	if dochange == 0:
+	if dochange == 1:
 		os.system("sudo scutil --set HostName " + mac)
 #Windows
 elif currentos == 'Windows':
 	print("Du bruger Windows")
 	mac = os.popen('wmic path win32_networkadapter where index=1 get MACAddress').read()
-	if dochange == 0:
+	if dochange == 1:
 		#CMD command for changing hostname
 		os.system('wmic computersystem where name="%COMPUTERNAME%" call rename name="' +mac + '"')
 		print(mac)
