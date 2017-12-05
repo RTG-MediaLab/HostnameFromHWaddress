4583adff0352ccf03cb40346d9010aadd4c7315c
Tobias Jensen <tobias@Pluto>
2017-12-05 20:22:52 +0100

    Updated aka.py

--- aka.py
+++ aka.py
@@ -1,10 +1,9 @@
-import csv
+from csv import DictReader
 
-alias = []
+
 f = open("alias.txt", r)
-for line in f:
-	line = line.strip()
-	alias.append(line)
+mac = [row["mac"] for row in DictReader(f)]
+alias = [row["aka"] for row in DictReader(f)]
 
 print alias
 
